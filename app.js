c1423a1deda39ae4288f13e865df62295cb16b57
hollyWillis <62303038+hollyWillis@users.noreply.github.com>
2020-05-05 12:27:41 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+
+function myFunction() {
+    var firstName = document.getElementById("firstName").value;
+    var email = document.getElementById("email").value;
+    alert(firstName + ", you have been signed up using " + email + ". Check your email for confirmation. Thank you very much :)");
+}
+
